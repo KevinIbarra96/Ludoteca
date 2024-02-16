5d091aa415b5d22bf3d927f81e00378b279e0bfa
raikishadows17 <gvacerox11@gmail.com>
2024-02-15 20:03:02 -0600

    Implemento validaciones de login

--- Desktop/Ludoteca/Ludoteca/Login.xaml.cs
+++ Desktop/Ludoteca/Ludoteca/Login.xaml.cs
@@ -6,35 +6,28 @@ public partial class Login : ContentPage
 	{
 		InitializeComponent();
 	}
-	private void OnLoginButtonClicked(object sender, EventArgs e)
+	
+	public void OnLoginButtonClicked(object sender, EventArgs e)
 	{
 		string username = txtUsername.Text;
 		string password = txtContraseña.Text;
 
-
-        
-        if (username == "GeraValente")
-        {
-            if (password == "test1")
-            {
-                // Si las credenciales son correctas, mostramos un mensaje de alerta
-                //Navigation.PushAsync(new AppShell());
-                
-                DisplayAlert("¡Éxito!", "Usuario " + username + " conectado correctamente.", "OK");
-                 Navigation.PushAsync(new AppShell());
-            }
-            else
-            {
-                // Si la contraseña es incorrecta, mostramos un mensaje de error específico
-                DisplayAlert("Error", "Contraseña incorrecta. Por favor, intente nuevamente.", "OK");
-            }
-        }
-        else
-        {
-            // Si el usuario es incorrecto, mostramos un mensaje de error específico
-            DisplayAlert("Error", "Usuario incorrecto. Por favor, intente nuevamente.", "OK");
-        }
-
-
+        string message= validacionLogin(username, password);
+		DisplayAlert("Mensaje", message, "OK");
+    }
+    public string validacionLogin(string username, string password)
+    {
+        string message = "";
+        if (username != "GerardoValente")
+		{
+			message = "Usuario no existe";
+		}
+		if(username == "GerardoValente" && password != "test1234")
+		{
+			message = "Contraseña incorrecta";
+		}
+			message = "Login exitoso";
+		
+		return message;
     }
 }
