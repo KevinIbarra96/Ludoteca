4cc1f3e293f7eb6b559dbe7b76b71e0f7f47e13e
Gerardo Valente <gvacerox11@gmail.com>
2024-12-18 22:51:28 -0600

    Actualizo MenuServices

--- ApiRest/Services/MenuService.php
+++ ApiRest/Services/MenuService.php
@@ -44,6 +44,46 @@
                 $x +=1;
             }
             $stm->execute();
+        }
+
+        function updateMenusForRol($idRole, $newMenuList){
+            $this->DbConection->beginTransaction();
+
+             // Obtener los menús actualmente asociados al rol
+            $query = "SELECT id_Menu FROM rol_menu WHERE id_Rol = :id_Rol";
+            $stm = $this->DbConection->prepare($query);
+            $stm->bindValue(':id_Rol', $idRole, PDO::PARAM_INT);
+            $stm->execute();
+            $currentMenuList = $stm->fetchAll(PDO::FETCH_COLUMN); 
+
+            // Determinar las relaciones a mantener, agregar y eliminar
+            $menusToAdd = array_diff($newMenuList, $currentMenuList);       // Menús nuevos
+            $menusToRemove = array_diff($currentMenuList, $newMenuList);   // Menús a eliminar
+            
+            //Eliminar las relacciones que ya no existen
+            if (!empty($menusToRemove)) {
+                $deleteQuery = "DELETE FROM rol_menu WHERE id_Rol = :id_Rol AND id_Menu = :id_Menu";
+                $stm = $this->DbConection->prepare($deleteQuery);
+                foreach ($menusToRemove as $menuId) {
+                    $stm->bindValue(':id_Rol', $idRole, PDO::PARAM_INT);
+                    $stm->bindValue(':id_Menu', $menuId, PDO::PARAM_INT);
+                    $stm->execute();
+                }
+            }
+
+            // Agregar nuevas relaciones
+            if (!empty($menusToAdd)) {
+                $insertQuery = "INSERT INTO rol_menu (id_Rol, id_Menu) VALUES (:id_Rol, :id_Menu)";
+                $stm = $this->DbConection->prepare($insertQuery);
+                foreach ($menusToAdd as $menuId) {
+                    $stm->bindValue(':id_Rol', $idRole, PDO::PARAM_INT);
+                    $stm->bindValue(':id_Menu', $menuId, PDO::PARAM_INT);
+                    $stm->execute();
+                    
+                }
+            }
+
+            $this->DbConection->commit();
 
         }
 
