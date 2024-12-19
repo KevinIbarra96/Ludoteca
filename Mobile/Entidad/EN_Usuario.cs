552e429eeb6a240d35a6f83e6ac2a32919b76aaf
Kevin Ibarra <kevin_daryl-1090@hotmail.com>
2024-12-18 23:45:42 -0600

    KDIS/Se creó el proyecto para la aplicacion mobil

---
+++ Mobile/Entidad/EN_Usuario.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidad
+{
+    internal class EN_Usuario : ModalBase
+    {
+        private int _id;
+
+        public int Id
+        {
+            get => _id;
+            set { _id = value; OnPropertyChanged(nameof(Id)); }
+        }
+    }
+}
---
+++ Mobile/LudotecaMobile/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace LudotecaMobile
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
